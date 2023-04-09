a543507432105bce48bd96fc78d7156ba5d575d5
Swasti Padhi <swasti.padhi@ibm.com>
2023-04-09 21:33:35 +0530

    First netflix commit

---
+++ src/Nav.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Nav.css';
+
+function Nav() {
+  return (
+    <div className='nav'>
+        <img src="https://www.edigitalagency.com.au/wp-content/uploads/Netflix-logo-red-black-png.png" alt="Netflix Logo" height={300}></img>
+        <img src="https://www.edigitalagency.com.au/wp-content/uploads/Netflix-logo-red-black-png.png" alt="Netflix Logo" height={300}></img>
+      
+      <h1>This is the nav</h1>
+    </div>
+  );
+}
+
+export default Nav;
---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCrkrv9MGeypNiqzeUNEn3LC2bXs97UPGk",
+    authDomain: "netflix-clone-ae805.firebaseapp.com",
+    projectId: "netflix-clone-ae805",
+    storageBucket: "netflix-clone-ae805.appspot.com",
+    messagingSenderId: "112931834428",
+    appId: "1:112931834428:web:581987046a32ed19f29139"
+  };
